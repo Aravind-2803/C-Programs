b1a36374d1d37f6587b0ed54e9789fec138c532a
Aravind280306 <aravindv2803@gmail.com>
2025-02-13 15:39:42 +0530

    Update students.c

--- students.c
+++ students.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct Student {
+typedef struct Student
+{
     int roll;
     char name[50];
     int age;
@@ -20,6 +21,8 @@ void dispOne(Student *arr, int n)
     for (int i = 0; i < n; i++)
         if (arr[i].roll == roll)
             printf("%d %s %d %c %d %d %d %s\n",arr[i].roll, arr[i].name, arr[i].age, arr[i].gender, arr[i].mark1, arr[i].mark2, arr[i].mark3, arr[i].grade);
+        else
+            printf("Not present \n");
 }
 
 void dispAll(Student *arr, int n)
@@ -38,80 +41,74 @@ void dispFail(Student *arr, int n)
 void dispPass(Student *arr, int n)
 {
     for (int i = 0; i < n; i++)
-        if (strncmp(arr[i].grade, "P", 2) != 0)
+        if (strncmp(arr[i].grade, "F", 2) != 0)
             printf("%d %s %d %c %d %d %d %s\n",arr[i].roll, arr[i].name, arr[i].age, arr[i].gender, arr[i].mark1, arr[i].mark2, arr[i].mark3, arr[i].grade);
 }
 
-void insert(Student *arr, int *n)
+void insert(Student *arr,int *n)
 {
-    if (*n == 99)
+  if(*n==99)
+    return;
+  Student students;
+  printf("Roll No: ");
+  scanf("%d", &students.roll);
+  printf("Name: ");
+  scanf("%s",students.name);
+  printf("Age: ");
+  scanf("%d", &students.age);
+  getchar();
+  printf("Enter The gender (M/F): ");
+  students.gender=getchar();
+  if(students.gender!='M'&&students.gender!='F')
+  {
+    printf("Invalid gender ");
+    return;
+  }
+  printf("Mark_1 (0-100): ");
+  scanf("%d", &students.mark1);
+  printf("Mark_2 (0-100): ");
+  scanf("%d", &students.mark2);
+  printf("Mark_3 (0-100): ");
+  scanf("%d", &students.mark3);
+  if ((students.mark1 < 0 || students.mark1 > 100) && (students.mark2 < 0 || students.mark2 > 100) && (students.mark3 < 0 || students.mark3 > 100))
         return;
-
-    Student student;
-    printf("Roll No: ");
-    scanf("%d", &student.roll);
-    getchar();
-    printf("Name: ");
-    fgets(student.name, 50, stdin);
-    student.name[strcspn(student.name, "\n")] = '\0';
-    char c;
-    while ((c = getchar()) != '\n' && c != EOF);
-    printf("Age: ");
-    scanf("%d", &student.age);
-    getchar();
-    printf("Gender (M/F): ");
-    student.gender = getchar();
-    printf("Mark1 (0-100): ");
-    scanf("%d", &student.mark1);
-    printf("Mark2 (0-100): ");
-    scanf("%d", &student.mark2);
-    printf("Mark3 (0-100): ");
-    scanf("%d", &student.mark3);
-
-    if (student.gender != 'M' && student.gender != 'F')
-        return;
-
-    if ((student.mark1 > 100 || student.mark1 < 0) && (student.mark2 > 100 || student.mark2 < 0) && (student.mark3 > 100 || student.mark3 < 0))
-        return;
-
-    if (student.mark1 < 50 || student.mark2 < 50 || student.mark3 < 50)
-        strcpy(student.grade, "F");
+  else
+  {
+    float avg=(students.mark1+students.mark2+students.mark3)/3;
+    if(avg>90)
+      strcpy(students.grade,"O");
+    else if (avg > 80 && avg <= 90)
+      strcpy(students.grade, "A+");
+    else if (avg > 70 && avg <= 80)
+      strcpy(students.grade, "A");
+    else if (avg > 60 && avg <= 70)
+      strcpy(students.grade, "B+");
+    else if (avg > 50 && avg <= 60)
+      strcpy(students.grade, "B");
+    else
+      strcpy(students.grade,"F");
+    }
+  for(int i=*n;i>-1;i--)
+  {
+    if(i==0)
+    {
+        arr[i]=students;
+    }
     else
     {
-        float avg = (student.mark1 + student.mark2 + student.mark3) / 3.f;
-        if (avg >= 50 && avg < 50)
-            strcpy(student.grade, "F");
-        else if (avg >= 50 && avg < 60)
-            strcpy(student.grade, "B");
-        else if (avg >= 60 && avg < 70)
-            strcpy(student.grade, "B+");
-        else if (avg >= 70 && avg < 80)
-            strcpy(student.grade, "A");
-        else if (avg >= 80 && avg < 90)
-            strcpy(student.grade, "A+");
-        else if (avg >= 90 && avg < 100 )
-            strcpy(student.grade, "O");
-        else
-            printf("Invalid Mark");
-
+      if(arr[i-1].roll>students.roll)
+          arr[i]=arr[i-1];
+      else
+      {
+        arr[i]=students;
+        break;
+      }
     }
-
-
-    for (int i = *n; i >= 0; i--) {
-        if (i == 0)
-            arr[i] = student;
-        else {
-            if (arr[i - 1].roll > student.roll)
-                arr[i] = arr[i - 1];
-            else {
-                arr[i] = student;
-                break;
-            }
-        }
     }
-    (*n)++;
-    dispAll(arr, *n);
+  (*n)++;
+  dispAll(arr,*n);
 }
+
 
 void delete(Student *arr, int *n)
 {
@@ -138,52 +135,22 @@ void delete(Student *arr, int *n)
 
 int main()
 {
-    Student students[100] = {};
-    int n, roll, choice;
-    int n;
-    printf("Enter the number of students: ");
-    scanf("%d", &n);
-    Student students[n];
-    for (int i = 0; i < n; i++)
-    {
-        printf("\nEnter details of student %d:\n", i + 1);
-        printf("Enter roll number: ");
-        scanf("%d", &students[i].roll);
-        printf("Enter name: ");
-        scanf("%s", students[i].name);
-        printf("Enter age: ");
-        scanf("%d", &students[i].age);
-        printf("Enter gender (M/F): ");
-        scanf(" %c", &students[i].gender);
-        printf("Enter mark 1: ");
-        scanf("%d", &students[i].mark1);
-        printf("Enter mark 2: ");
-        scanf("%d", &students[i].mark2);
-        printf("Enter mark 3: ");
-        scanf("%d", &students[i].mark3);
-        printf("Enter grade (A/B/C): ");
-        scanf("%s", students[i].grade);
-    }
-    printf("\nStudent Details:\n");
-    for (int i = 0; i < n; i++)
-    {
-        printf("Student %d:\n", i + 1);
-        printf("Roll Number: %d\n", students[i].roll);
-        printf("Name: %s\n", students[i].name);
-        printf("Age: %d\n", students[i].age);
-        printf("Gender: %c\n", students[i].gender);
-        printf("Mark 1: %d\n", students[i].mark1);
-        printf("Mark 2: %d\n", students[i].mark2);
-        printf("Mark 3: %d\n", students[i].mark3);
-        printf("Grade: %s\n\n", students[i].grade);
-    }
+    int init,n, roll, choice,avg,grade;
     n = 0;
     choice = 0;
+    printf("Enter the number of students: ");
+    scanf("%d", &init);
+    Student students[100];
+    for (int i = 0; i < init; i++)
+    {
+        insert(students, &n);
+    }
+    
     while (1)
     {
         printf("--------------------\n");
         printf("1.Display\n2.Insert\n3.Delete\n4.Exit\nEnter Your Choice: ");
-        scanf("%i", &choice);
+        scanf("%d", &choice);
 
         switch (choice) {
         case 1:
@@ -191,7 +158,8 @@ int main()
             printf("1.All\n2.One\nEnter Your Choice: ");
             scanf("%d", &choice);
 
-            switch (choice) {
+            switch (choice)
+            {
             case 1:
                 printf("1.All\n2.Passed\n3.Failed\nEnter Your Choice: ");
                 scanf("%d", &choice);
@@ -230,7 +198,9 @@ int main()
             break;
 
         case 4:
+            printf("_______________________________________\n");
             printf("THANK YOU...\n");
+            printf("_______________________________________\n");
             return 0;
 
         default:
